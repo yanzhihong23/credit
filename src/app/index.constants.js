515562099134d9689535798363eab45af8c864e7
Hom <yanzhihong23@qq.com>
2015-09-24 22:59:45 +0800

    update merchant id

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -4,7 +4,7 @@
   angular
     .module('credit')
     .constant('HOST', 'http://openapi.test.nonobank.com')
-    .constant('MERCHANT', '10002')
+    .constant('MERCHANT', '1291795')
     .constant('PRODUCTID', '90')
     .factory('OPENID', function($location, utils, $log) {
       $log.debug('factory');
